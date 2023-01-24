98e65dd98d3e839b57d22c71535781cd41d104e2
Cristian Duque <duquec@sunypoly.edu>
2023-01-24 17:19:47 -0500

    Additional task

--- assign01/src/main/java/edu/duquec/assign01/Greetings.java
+++ assign01/src/main/java/edu/duquec/assign01/Greetings.java
@@ -3,6 +3,7 @@ package edu.duquec.assign01;
 public class Greetings {
     public static void main(String [] args){
         System.out.println("Welcome to DarkTide");
+        System.out.println("For the God Emperor");
         //Prints out msg DarkTide
     }
 }
