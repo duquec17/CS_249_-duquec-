b3370abc5bfee3c2cb3e1bbc048bf288a41ab087
Cristian Duque <duquec@sunypoly.edu>
2023-02-02 17:01:22 -0500

    exercise 03

--- exercises03/src/main/java/edu/duquec/exercises03/FoodRationer.java
+++ exercises03/src/main/java/edu/duquec/exercises03/FoodRationer.java
@@ -1,14 +1,23 @@
 package edu.duquec.exercises03;
+import java.util.Scanner;
 
 public class FoodRationer {
     public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to the trail!");
 
-        double totalFood = 100;
-        int partyCnt = 5;
+        System.out.println("Enter total food:");
+        double totalFood = input.nextDouble();
+        System.out.println("Enter number of people:");
+        int partyCnt = input.nextInt();
         double rations = 10;
 
         double daysLeft = totalFood/(partyCnt*rations);
         System.out.println("DAYS LEFT: " + daysLeft);
+
+        double y =2.0;
+        int x = 10;
+        x = (int)(x/y);
     }
 }
