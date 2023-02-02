dccaf0c002c1340855e54985097ab3aafd410368
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-02-02 17:59:30 -0500

    Added some example code (some of which doesn't work to demonstrate a point)

--- profexercises03/src/main/java/edu/realemj/exercises03/FoodRationer.java
+++ profexercises03/src/main/java/edu/realemj/exercises03/FoodRationer.java
@@ -1,15 +1,39 @@
 package edu.realemj.exercises03;
 
+import java.util.Scanner;
+
 public class FoodRationer {
     public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to the trail!");
 
-        double totalFood = 100;
-        int partyCnt = (int)5.0;
+        System.out.println("Enter total food:");
+        double totalFood = input.nextDouble();
+        System.out.println("Enter number of people:");
+        int partyCnt = input.nextInt();
         double rations = 10;
 
         double daysLeft = totalFood/(partyCnt*rations);
         System.out.println("DAYS LEFT: " + daysLeft);
+
+        double y = 2.0;
+        int x = 10;
+        x = (int)(x/y);
+
+        String t = null;
+
+        if(t != null) {
+            System.out.println(t);
+            System.out.println(t.length());
+        }
+
+        String s = "";
+        System.out.println(s);
+        System.out.println(s.length());
+
+        char c = s.charAt(0);
+
 
     }
 }
