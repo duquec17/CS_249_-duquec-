1f42afe50d72308410ecb6f6367abdad00a497a9
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-02-09 17:52:58 -0500

    Starting on General Store

---
+++ profexercises03/src/main/java/edu/realemj/exercises03/L33tSpeak.java
@@ -0,0 +1,35 @@
+package edu.realemj.exercises03;
+
+import java.util.*;
+
+public class L33tSpeak {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        String line = "";
+        do {
+            System.out.println("Enter message:");
+            line = input.nextLine();
+            line = line.trim();
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+
+                char nc = switch(c) {
+                    case 'a','A' -> '4';
+                    case 'e','E' -> '3';
+                    case 'i','I' -> '1';
+                    case 'o','O' -> '0';
+                    default -> c;
+                };
+
+                sb.append(nc);
+            }
+
+            System.out.println(sb.toString());
+
+        }while(line.length() != 0);
+
+    }
+}
