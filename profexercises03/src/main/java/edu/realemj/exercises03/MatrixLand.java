deaba1ad7cebc573bdf71b86674b14e5e2fe01c4
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-02-21 18:20:49 -0500

    Did matrix example

---
+++ profexercises03/src/main/java/edu/realemj/exercises03/MatrixLand.java
@@ -0,0 +1,14 @@
+package edu.realemj.exercises03;
+
+public class MatrixLand {
+    public static void main(String [] args) {
+        Matrix2D m = Matrix2D.makeTranslate2D(2, 4);
+
+        Point2D p = new Point2D(3, 14);
+
+        Point2D q = m.multiply(p);
+
+        System.out.println("p = " + p);
+        System.out.println("q = " + q);
+    }
+}
---
+++ profexercises03/src/main/java/edu/realemj/exercises03/Point2D.java
@@ -0,0 +1,42 @@
+package edu.realemj.exercises03;
+
+public class Point2D {
+    private double x;
+    private double y;
+
+    public Point2D() {}
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+
+    public double get(int index) {
+        double val = switch(index) {
+            case 0 -> x;
+            case 1 -> y;
+            case 2 -> 1;
+            default -> 0;
+        };
+        return val;
+    }
+
+    public void set(int index, double val) {
+        switch(index) {
+            case 0 -> x = val;
+            case 1 -> y = val;
+            default -> {}
+        }
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+}
