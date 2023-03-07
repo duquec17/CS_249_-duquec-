ef49ec9d259724bbce6cf03d04a77a1dc8a34e10
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-03-07 17:50:57 -0500

    Added Shape stuff

---
+++ profexercises/src/main/java/edu/realemj/exercises12/ShapeLand.java
@@ -0,0 +1,10 @@
+package edu.realemj.exercises12;
+
+import edu.realemj.math.*;
+public class ShapeLand {
+    public static void main(String [] args) {
+
+        Circle c = new Circle();
+        Point2D p = c.getCenter();
+    }
+}
