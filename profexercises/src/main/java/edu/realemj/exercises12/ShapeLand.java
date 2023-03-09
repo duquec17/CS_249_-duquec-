97b42330860756afa175f794ef894ad373424ebd
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-03-09 18:11:08 -0500

    Inheritance exercises

--- profexercises/src/main/java/edu/realemj/exercises12/ShapeLand.java
+++ profexercises/src/main/java/edu/realemj/exercises12/ShapeLand.java
@@ -6,5 +6,29 @@ public class ShapeLand {
 
         Circle c = new Circle();
         Point2D p = c.getCenter();
+
+        Circle c2 = new Circle(8.9, new Point2D(2,3));
+
+        System.out.println(c2);
+
+        Shape s = new Circle();
+        System.out.println("BEFORE: " + s);
+        s = new Shape();
+        System.out.println("AFTER: " + s);
+
+        Shape [] allShapes = new Shape[5];
+        for(int i = 0; i < allShapes.length; i++) {
+            if(i%2 == 0) {
+                allShapes[i] = new Shape();
+            }
+            else {
+                allShapes[i] = new Circle();
+            }
+        }
+
+        System.out.println("ALL SHAPES:");
+        for(Shape myShape: allShapes) {
+            System.out.println(myShape);
+        }
     }
 }
