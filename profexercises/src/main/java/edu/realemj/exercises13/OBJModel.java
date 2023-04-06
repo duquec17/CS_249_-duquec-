058d6e74a4c1209a10179f753b7af4c59648cd51
Dr. Michael J. Reale <realemj@sunypoly.edu>
2023-04-06 17:06:22 -0400

    POSTED ASSIGNMENT 05 FILES (also exercises)

--- profexercises/src/main/java/edu/realemj/exercises13/OBJModel.java
+++ profexercises/src/main/java/edu/realemj/exercises13/OBJModel.java
@@ -2,7 +2,61 @@ package edu.realemj.exercises13;
 
 import edu.realemj.math.*;
 import java.util.*;
+import java.io.*;
+
 public class OBJModel {
     private ArrayList<Point3D> vertices = new ArrayList<>();
+    private ArrayList<Integer> indices = new ArrayList<>();
 
+    public void save(String filename) throws IOException {
+        try (
+                PrintWriter writer = new PrintWriter(filename);
+                ) {
+            for(Point3D v: vertices) {
+                writer.println(v.objString());
+            }
+
+            for(int i = 0; i < indices.size(); i += 3) {
+                int index0 = indices.get(i);
+                int index1 = indices.get(i+1);
+                int index2 = indices.get(i+2);
+                writer.println("f " + index0
+                                + " " + index1
+                                + " " + index2);
+            }
+        }
+    }
+
+    public void load(String filename) throws IOException {
+        try (
+                Scanner fileInput = new Scanner(new File(filename));
+                ) {
+
+            vertices.clear();
+            indices.clear();
+
+            while(fileInput.hasNextLine()) {
+                String line = fileInput.nextLine();
+                line = line.trim();
+                if(line.length() > 0) {
+                    Scanner tokens = new Scanner(line);
+                    String first = tokens.next();
+                    if(first.equals("v")) {
+                        // Vertex
+                        double x = tokens.nextDouble();
+                        double y = tokens.nextDouble();
+                        double z = tokens.nextDouble();
+                        Point3D v = new Point3D(x, y, z);
+                        vertices.add(v);
+                    }
+                    else if(first.equals("f")) {
+                        // Face
+                        indices.add(tokens.nextInt());
+                        indices.add(tokens.nextInt());
+                        indices.add(tokens.nextInt());
+                    }
+                }
+            }
+        }
+    }
 }
